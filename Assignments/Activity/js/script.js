7107309cff3333966796a8bdd904ca2653386808
Tony LindoRock <yichen@yichens-mbp.wireless.concordia.ca>
2020-01-07 17:09:21 -0500

    Activity: some minor changes

--- Assignments/Activity/js/script.js
+++ Assignments/Activity/js/script.js
@@ -8,10 +8,13 @@ author, and this description to match your project!
 
 ******************/
 
+let start = false;
+let score = 0;
+
 let player = {
   posX: 0,
   posY: 0,
-  maxSize: 50,
+  MAX_SIZE: 50,
   currentSize: 50,
   dead: false
 };
@@ -41,24 +44,34 @@ function setupFood(){
 
 function draw() {
   background("#262626");
-  if (!player.dead){
-    displayFood();
-    displayPlayer();
-    updatePlayerSize();
+  if (start){
+    if (!player.dead){
+      displayFood();
+      displayPlayer();
+      updatePlayerSize();
 
-    playing();
+      playing();
+    }
+  }else{
+    displayPlayer();
+    displayStartMenu();
   }
+  displayScore();
 }
 
 function playing(){
   let d = dist(mouseX,mouseY,food.posX,food.posY);
   if (d < player.currentSize/2 + food.size/2){
     player.currentSize += 20;
-    player.currentSize = constrain(player.currentSize,0,player.maxSize);
+    player.currentSize = constrain(player.currentSize,0,player.MAX_SIZE);
     setupFood();
+
+    score ++;
   }
   if (player.currentSize <= 0){
     player.dead = true;
+    start = false;
+    resetPlayer();
   }
 }
 
@@ -76,7 +89,7 @@ function displayPlayer(){
 function updatePlayerSize(){
   if (player.currentSize > 0){
     player.currentSize -= 0.45;
-    player.currentSize = constrain(player.currentSize,0,player.maxSize);
+    player.currentSize = constrain(player.currentSize,0,player.MAX_SIZE);
   }
 }
 
@@ -87,4 +100,34 @@ function displayFood(){
   fill(0,255,0);
   ellipse(food.posX, food.posY, food.size);
   pop();
+}
+
+function displayScore(){
+  push();
+  fill(255);
+  textSize(64);
+  textStyle(BOLD);
+  textAlign(CENTER,CENTER);
+  text(score, windowWidth/2,75);
+  pop();
+}
+
+function displayStartMenu(){
+  push();
+  fill(255);
+  textSize(64);
+  textStyle(BOLD);
+  textAlign(CENTER,CENTER);
+  text("CLICK TO PLAY!", windowWidth/2,windowHeight/2);
+  pop();
+  if (mouseIsPressed){
+    resetPlayer();
+    start = true;
+    score = 0;
+  }
+}
+
+function resetPlayer(){
+  player.currentSize = player.MAX_SIZE;
+  player.dead = false;
 }
