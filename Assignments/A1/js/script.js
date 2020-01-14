254033b854fb699265a181f79bd7f1393fe4f2cc
Tony LindoRock <yichen@yichens-mbp.wireless.concordia.ca>
2020-01-14 17:17:46 -0500

    A1: Added random color & remove()

--- Assignments/A1/js/script.js
+++ Assignments/A1/js/script.js
@@ -9,14 +9,18 @@ You paint in pixels!
 
 *********************************************************************/
 
-window.onload = setup;
-const PIXEL_SIZE = 24;
+window.onload = setup; // call setup when the website is loaded
+const PIXEL_SIZE = 24; // the pixel size
 
+// setup
+//
+// to be called when first loaded
+// will set up the pixel canvas and add event listener
 function setup(){
   console.log("Website is loaded! Weeeeee!");
 
-  let pixelAmountX = parseInt(window.innerWidth/PIXEL_SIZE);
-  let pixelAmountY = parseInt(window.innerHeight/PIXEL_SIZE);
+  let pixelAmountX = Math.floor(window.innerWidth/PIXEL_SIZE);
+  let pixelAmountY = Math.floor(window.innerHeight/PIXEL_SIZE);
 
   for(let i = 0; i < pixelAmountY; i++){
     for(let j = 0; j < pixelAmountX; j++){
@@ -28,16 +32,28 @@ function setup(){
   console.log("The window size is: "+window.innerWidth+" * "+window.innerHeight+"\nThe pixel canvas size is : "+pixelAmountX+" * "+pixelAmountY);
 
   document.addEventListener('mouseover',paint);
+  document.addEventListener('click',remove);
 }
 
 function paint(e){
   let pixel = e.target;
   if (pixel.className === "pixel"){
-    pixel.style.backgroundColor = 'orange';
+    let colorR = Math.floor((Math.random() * 255) + 1);
+    let colorG = Math.floor((Math.random() * 255) + 1);
+    let colorB = Math.floor((Math.random() * 255) + 1);
+    pixel.style.backgroundColor = "rgb("+colorR+","+colorG+","+colorB+")";
     setTimeout(resetPixel,1000,pixel);
+    console.log("rbg("+colorR+","+colorG+","+colorB+")");
   }
 }
 
 function resetPixel(pixel){
   pixel.style.backgroundColor = 'black';
+}
+
+function remove(e){
+  let pixel = e.target;
+  if (pixel.className === "pixel"){
+    pixel.style.opacity = '0';
+  }
 }
