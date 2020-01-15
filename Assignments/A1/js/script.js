30b2774dbd4eb23b58b341f0509937a6c3fcb34f
Tony LindoRock <yichen@Yichens-MacBook-Pro.local>
2020-01-14 22:12:08 -0500

    A1: Added a lot of buttons

--- Assignments/A1/js/script.js
+++ Assignments/A1/js/script.js
@@ -15,6 +15,12 @@ const PIXEL_SIZE = 24; // the pixel size
 let rotation = 0; // global rotation
 let currentKey = ""; // global key
 
+let drawing = false;
+let keepingPixel = false;
+let erasing = false;
+let currentColor = -1;
+let lastColor = -1;
+
 // setup
 //
 // to be called when first loaded
@@ -39,16 +45,138 @@ function setup(){
   document.addEventListener('keydown',rotate);
   document.addEventListener('keydown',typed);
   document.addEventListener('mouseover',addText);
+
+  document.addEventListener('mousedown',e =>{
+    drawing = true;
+  });
+  document.addEventListener('mouseup',e =>{
+    if (drawing){
+      drawing = false;
+    }
+  });
+
+  let keepPixel = document.getElementById('btn-keep');
+  keepPixel.addEventListener('click',e =>{
+    keepingPixel = !keepingPixel;
+    if (keepingPixel){
+      keepPixel.style.backgroundColor = "#262626";
+    }else{
+      keepPixel.style.backgroundColor = "black";
+    }
+  })
+
+  let eraser = document.getElementById('btn-erase');
+  eraser.addEventListener('click',e =>{
+    erasing = !erasing;
+    if (erasing){
+      eraser.style.backgroundColor = "#262626";
+    }else{
+      eraser.style.backgroundColor = "black";
+    }
+  })
+
+  let clear = document.getElementById('btn-clear');
+  clear.addEventListener('click',e =>{
+    clearAll();
+  })
+
+  let random = document.getElementById('btn-random');
+  random.style.backgroundColor = "#262626";
+  random.addEventListener('click',e =>{
+    lastColor = currentColor;
+    currentColor = -1;
+    updateSelection();
+  })
+
+  let red = document.getElementById('btn-r');
+  red.addEventListener('click',e =>{
+    lastColor = currentColor;
+    currentColor = 0;
+    updateSelection();
+  })
+
+  let orange = document.getElementById('btn-o');
+  orange.addEventListener('click',e =>{
+    lastColor = currentColor;
+    currentColor = 1;
+    updateSelection();
+  })
+
+  let yellow = document.getElementById('btn-y');
+  yellow.addEventListener('click',e =>{
+    lastColor = currentColor;
+    currentColor = 2;
+    updateSelection();
+  })
+
+  let green = document.getElementById('btn-g');
+  green.addEventListener('click',e =>{
+    lastColor = currentColor;
+    currentColor = 3;
+    updateSelection();
+  })
+
+  let cyan = document.getElementById('btn-c');
+  cyan.addEventListener('click',e =>{
+    lastColor = currentColor;
+    currentColor = 4;
+    updateSelection();
+  })
+
+  let blue = document.getElementById('btn-b');
+  blue.addEventListener('click',e =>{
+    lastColor = currentColor;
+    currentColor = 5;
+    updateSelection();
+  })
+
+  let purple = document.getElementById('btn-p');
+  purple.addEventListener('click',e =>{
+    lastColor = currentColor;
+    currentColor = 6;
+    updateSelection();
+  })
 }
 
 function paint(e){
-  let pixel = e.target;
-  if (pixel.className === "pixel"){
-    let colorR = Math.floor((Math.random() * 255) + 1);
-    let colorG = Math.floor((Math.random() * 255) + 1);
-    let colorB = Math.floor((Math.random() * 255) + 1);
-    pixel.style.backgroundColor = "rgb("+colorR+","+colorG+","+colorB+")";
-    setTimeout(resetPixel,1000,pixel);
+  if (drawing){
+    let pixel = e.target;
+    if (pixel.className === "pixel"){
+      if (!erasing){
+        if (currentColor < 0){
+          let colorR = Math.floor((Math.random() * 255) + 1);
+          let colorG = Math.floor((Math.random() * 255) + 1);
+          let colorB = Math.floor((Math.random() * 255) + 1);
+          pixel.style.backgroundColor = "rgb("+colorR+","+colorG+","+colorB+")";
+        // red
+        }else if (currentColor === 0){
+          pixel.style.backgroundColor = "rgb(255,0,0)";
+        // orange
+        }else if (currentColor === 1){
+          pixel.style.backgroundColor = "rgb(255,132,0)";
+        // yellow
+        }else if (currentColor === 2){
+          pixel.style.backgroundColor = "rgb(255,232,0)";
+        // green
+        }else if (currentColor === 3){
+          pixel.style.backgroundColor = "rgb(0,255,0)";
+        // cyan
+        }else if (currentColor === 4){
+          pixel.style.backgroundColor = "rgb(0,255,232)";
+        // blue
+        }else if (currentColor === 5){
+          pixel.style.backgroundColor = "rgb(0,0,255)";
+        // purple
+        }else if (currentColor === 6){
+          pixel.style.backgroundColor = "rgb(255,0,232)";
+        }
+      }else{
+        pixel.style.backgroundColor = "black";
+      }
+      if (!keepingPixel){
+        setTimeout(resetPixel,1000,pixel);
+      }
+    }
   }
 }
 
@@ -83,5 +211,74 @@ function addText(e){
   let pixel = e.target;
   if (pixel.className === "pixel"){
     pixel.innerHTML = String.fromCharCode(currentKey);
+  }
+}
+
+function updateSelection(){
+  let random = document.getElementById('btn-random');
+  let red = document.getElementById('btn-r');
+  let orange = document.getElementById('btn-o');
+  let yellow = document.getElementById('btn-y');
+  let green = document.getElementById('btn-g');
+  let cyan = document.getElementById('btn-c');
+  let blue = document.getElementById('btn-b');
+  let purple = document.getElementById('btn-p');
+
+  if (lastColor < 0){
+    random.style.backgroundColor = "black";
+  // red
+  }else if (lastColor === 0){
+    red.style.backgroundColor = "black";
+  // orange
+  }else if (lastColor === 1){
+    orange.style.backgroundColor = "black";
+  // yellow
+  }else if (lastColor === 2){
+    yellow.style.backgroundColor = "black";
+  // green
+  }else if (lastColor === 3){
+    green.style.backgroundColor = "black";
+  // cyan
+  }else if (lastColor === 4){
+    cyan.style.backgroundColor = "black";
+  // blue
+  }else if (lastColor === 5){
+    blue.style.backgroundColor = "black";
+  // purple
+  }else if (lastColor === 6){
+    purple.style.backgroundColor = "black";
+  }
+
+  if (currentColor < 0){
+    random.style.backgroundColor = "#262626";
+  // red
+  }else if (currentColor === 0){
+    red.style.backgroundColor = "rgba(255,0,0,0.5)";
+  // orange
+  }else if (currentColor === 1){
+    orange.style.backgroundColor = "rgba(255,132,0,0.5)";
+  // yellow
+  }else if (currentColor === 2){
+    yellow.style.backgroundColor = "rgba(255,232,0,0.5)";
+  // green
+  }else if (currentColor === 3){
+    green.style.backgroundColor = "rgba(0,255,0,0.5)";
+  // cyan
+  }else if (currentColor === 4){
+    cyan.style.backgroundColor = "rgba(0,255,232,0.5)";
+  // blue
+  }else if (currentColor === 5){
+    blue.style.backgroundColor = "rgba(0,0,255,0.5)";
+  // purple
+  }else if (currentColor === 6){
+    purple.style.backgroundColor = "rgba(255,0,232,0.5)";
+  }
+}
+
+function clearAll(){
+  let pixels = document.querySelectorAll('.pixel');
+  for(let i = 0; i < pixels.length; i++){
+    pixels[i].style.backgroundColor = "black";
+    pixels[i].style.opacity = "1";
   }
 }
