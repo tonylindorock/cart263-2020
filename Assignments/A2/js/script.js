d0fc9f1dc5c00bd6b3a92247c6250cbaba183d90
Tony LindoRock <yichen@yichens-mbp.wireless.concordia.ca>
2020-01-21 17:14:08 -0500

    A2: Added an ending

--- Assignments/A2/js/script.js
+++ Assignments/A2/js/script.js
@@ -14,35 +14,56 @@ let $secrets;
 let secretsFound = 0;
 let secretsTotal;
 
+let NUM_REDACTED = 13
+let revealed = 0;
+
+let UPDATE_TIME = 500;
+
+let timer;
+
 $(document).ready(setup);
 
 function setup() {
   $spans = $("span");
   $secrets = $(".secret");
+
+  $(".ending").hide();
+
   console.log("Weeeee!");
 
   secretsTotal = $secrets.length;
   $("#secrets-total").text(secretsTotal);
   $secrets.on('mouseover',onMouserover);
 
-  setInterval(update,500);
+  timer = setInterval(update,UPDATE_TIME);
   $spans.on('click',spanClicked);
 }
 
 function update(){
   $spans.not(".secret").not(".found").not(".ui").each(updateSpan);
   console.log("Updated!");
+
+  if (revealed === NUM_REDACTED){
+    gameOver();
+    clearInterval(timer);
+  }
 }
 
 function updateSpan(){
   let p = Math.random();
   if (p < 0.1){
-    $(this).removeClass("redacted").addClass("revealed");
+    if ($(this).hasClass("redacted")){
+      $(this).removeClass("redacted").addClass("revealed");
+      revealed++;
+    }
   }
   console.log("Span updated!");
 }
 
 function spanClicked(){
+  if ($(this).hasClass("revealed")){
+    revealed--;
+  }
   $(this).not(".found").removeClass("revealed").addClass("redacted");
 }
 
@@ -51,4 +72,9 @@ function onMouserover(){
   $(this).off('mouseover');
   secretsFound++;
   $("#secrets-found").text(secretsFound);
+}
+
+function gameOver(){
+  $("p").slideToggle(UPDATE_TIME);
+  $(".ending").show();
 }
