6364197b8ba54b8689028498da02ee82f5c0e001
Tony LindoRock <yichen@yichens-mbp.wireless.concordia.ca>
2020-01-28 13:54:27 -0500

    P1: Added a draggable square

--- Projects/P1/js/script.js
+++ Projects/P1/js/script.js
@@ -11,8 +11,29 @@ to match your project! Write JavaScript to do amazing things below!
 *********************************************************************/
 
 $(document).ready(setup);
+let $square;
 
-function setup() {
+function setup(){
   console.log("Weeee!");
-  $(".draggable").draggable();
+
+  $square = $(".draggable");
+
+  $square.draggable();
+  $square.on('mousedown',dragging);
+  $square.on('mouseup',reset);
+  $square.on('dragstop',reset);
+}
+
+function dragging(){
+  $square.css({
+    cursor: 'grabbing'
+  });
+}
+
+function reset(){
+  $square.css({
+    top: '40%',
+    left: '15%',
+    cursor: 'grab'
+  });
 }
