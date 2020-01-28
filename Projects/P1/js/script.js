0501adeb0f22f21c97185299719d2b775efef0d9
Tony LindoRock <yichen@Yichens-MacBook-Pro.local>
2020-01-27 20:02:47 -0500

    P1: Started Project 1

---
+++ Projects/P1/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/********************************************************************
+
+Title of Project
+Yichen Wang
+
+This is a template. Fill in the title, author, and this description
+to match your project! Write JavaScript to do amazing things below!
+
+*********************************************************************/
+
+$(document).ready(setup);
+
+function setup() {
+
+}
