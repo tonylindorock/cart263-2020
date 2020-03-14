110c063e30daf749e502dd5ca5a7895b659df23d
Tony LindoRock <yichen@Yichens-MacBook-Pro.local>
2020-03-14 16:29:51 -0400

    A4: Fixed the "a"/"an" issue and composed the phrase

--- Assignments/A4/js/script.js
+++ Assignments/A4/js/script.js
@@ -9,42 +9,90 @@ A random sentence?
 
 ******************/
 
+let finalPhrase = "";
+
+let randomCondiment;
+let tea;
+let vegetable;
+let verb = "is";
+let color;
+let cat;
+let preposition = " in ";
+let room;
+
 $(document).ready(setup);
 
-function setup(){
-  $.getJSON("Data/Data.json")
-  .done(dataLoaded)
-  .fail(dataError);
-  $.getJSON("Data/Crayola.json")
-  .done(dataLoaded)
-  .fail(dataError);
-  $.getJSON("Data/Tea.json")
-  .done(dataLoaded)
-  .fail(dataError);
-  $.getJSON("Data/Vegetables.json")
-  .done(dataLoaded)
-  .fail(dataError);
+function setup() {
+  $.when(
+    $.getJSON("Data/Crayola.json")
+    .done(crayolaLoaded)
+    .fail(dataError),
+
+    $.getJSON("Data/Data.json")
+    .done(dataLoaded)
+    .fail(dataError),
+
+    $.getJSON("Data/Tea.json")
+    .done(teaLoaded)
+    .fail(dataError),
+
+    $.getJSON("Data/Vegetables.json")
+    .done(vegetabelLoaded)
+    .fail(dataError)
+  ).then(allFilesLoaded);
 }
 
-function dataLoaded(data){
-  let randomCondiment = getRandomElement(data.condiments);
+function dataLoaded(data) {
+  randomCondiment = getRandomElement(data.condiments);
 
-  let verb = "is";
-  if (randomCondiment.charAt(randomCondiment.length - 1) === "s"){
+  if (randomCondiment.charAt(randomCondiment.length - 1) === "s") {
     verb = "are";
   }
-  let cat = getRandomElement(data.cats);
-  let room = getRandomElement(data.rooms);
-
-  let phrase = randomCondiment+" "+verb+" "+cat+" in a "+room+".";
-  $('body').append(phrase);
+  cat = getRandomElement(data.cats);
+  if (verb === "are") {
+    cat += "s";
+  }
+  room = getRandomElement(data.rooms);
+  let firstLetter = room.toLowerCase().charAt(0);
+  if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u") {
+    preposition += "an ";
+  } else {
+    preposition += "a ";
+  }
 }
 
-function dataError(request, text, error){
+function crayolaLoaded(data) {
+  color = getRandomElement(data.colors).color;
+  let firstLetter = color.toLowerCase().charAt(0);
+  if (firstLetter === "a" || firstLetter === "e" || firstLetter === "i" || firstLetter === "o" || firstLetter === "u") {
+    if (verb === "is") {
+      verb += " an ";
+    }
+  } else {
+    if (verb === "is") {
+      verb += " a ";
+    }
+  }
+}
+
+function teaLoaded(data) {
+  tea = getRandomElement(data.teas);
+}
+
+function vegetabelLoaded(data) {
+  vegetable = getRandomElement(data.vegetables)
+}
+
+function dataError(request, text, error) {
   console.log(error);
 }
 
-function getRandomElement(array){
-  let element = array[Math.floor(Math.random()*array.length)];
+function allFilesLoaded() {
+  finalPhrase = "With a cup of " + tea + " and " + vegetable + ", " + randomCondiment + " " + verb + color + cat + preposition + room + ".";
+  $('body').append(finalPhrase)
+}
+
+function getRandomElement(array) {
+  let element = array[Math.floor(Math.random() * array.length)];
   return element
 }
