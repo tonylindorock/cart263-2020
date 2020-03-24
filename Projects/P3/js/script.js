d005577702078dd839dc0c614212df187c040b35
Tony LindoRock <yichen@Yichens-MacBook-Pro.local>
2020-03-24 14:22:43 -0400

    P3: started project 3

---
+++ Projects/P3/js/script.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/********************************************************************
+
+Project 3
+Yichen Wang
+
+This is a template. Fill in the title, author, and this description
+to match your project! Write JavaScript to do amazing things below!
+
+*********************************************************************/
+
+$(document).ready(setup);
+
+function setup() {
+
+}
