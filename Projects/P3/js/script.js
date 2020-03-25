349afe4d6fd54f48a7b523827062d15ee5ac8af7
Tony LindoRock <yichen@Yichens-MacBook-Pro.local>
2020-03-25 15:23:24 -0400

    P3: Added a main menu with an animation

--- Projects/P3/js/script.js
+++ Projects/P3/js/script.js
@@ -3,15 +3,57 @@
 /********************************************************************
 
 Project 3
+The Art of Being Artistic
 Yichen Wang
 
-This is a template. Fill in the title, author, and this description
-to match your project! Write JavaScript to do amazing things below!
-
+FONT FROM: https://www.urbanfonts.com/fonts/superscript.htm
 *********************************************************************/
 
-$(document).ready(setup);
+let mainMenuFontHeight;
+let mainMenuFontAlpha = 0;
+
+// current state of the program
+let state = "START";
+
+// to store font
+let themeFont;
+
+const BG_COLOR = "#111";
+
+function preload() {
+  themeFont = loadFont("assets/superscript/SUPERSCR.TTF");
+}
 
 function setup() {
+  createCanvas(windowWidth, windowHeight);
+  background(BG_COLOR);
+  textFont(themeFont);
+  textStyle(BOLD);
+  textSize(16);
+  textAlign(CENTER,CENTER);
+  rectMode(CENTER);
+  ellipseMode(CENTER);
 
+  setupMainMenu();
+}
+
+function draw() {
+  background(BG_COLOR);
+  if state === "START"{
+    displayMainMenu();
+  }
+}
+
+function setupMainMenu(){
+  mainMenuFontHeight = height;
+}
+
+function displayMainMenu() {
+  push();
+  textSize(64);
+  mainMenuFontAlpha = lerp(mainMenuFontAlpha, 255, 0.05);
+  mainMenuFontHeight = lerp(mainMenuFontHeight, height/2, 0.05);
+  fill(255,mainMenuFontAlpha);
+  text("The Art of\nBeing Artistic",width/2,mainMenuFontHeight);
+  pop();
 }
