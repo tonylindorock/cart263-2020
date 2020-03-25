bc8f3e497438c8fa695d3f15c0d5fc6a7f417694
Tony LindoRock <yichen@Yichens-MacBook-Pro.local>
2020-03-25 16:26:53 -0400

    P3: Minor changes

--- Projects/P3/js/script.js
+++ Projects/P3/js/script.js
@@ -9,8 +9,10 @@ Yichen Wang
 FONT FROM: https://www.urbanfonts.com/fonts/superscript.htm
 *********************************************************************/
 
+// the font attributes of title for the animation
 let mainMenuFontHeight;
 let mainMenuFontAlpha = 0;
+let doOnce = true;
 
 // current state of the program
 let state = "START";
@@ -18,10 +20,11 @@ let state = "START";
 // to store font
 let themeFont;
 
+// the color of background
 const BG_COLOR = "#111";
 
 function preload() {
-  themeFont = loadFont("assets/superscript/SUPERSCR.TTF");
+  themeFont = loadFont("assets/font/SUPERSCR.TTF");
 }
 
 function setup() {
@@ -39,21 +42,29 @@ function setup() {
 
 function draw() {
   background(BG_COLOR);
-  if state === "START"{
+  if (state === "START"){
     displayMainMenu();
   }
 }
 
 function setupMainMenu(){
   mainMenuFontHeight = height;
+  mainMenuFontAlpha = 0;
 }
 
 function displayMainMenu() {
   push();
   textSize(64);
   mainMenuFontAlpha = lerp(mainMenuFontAlpha, 255, 0.05);
-  mainMenuFontHeight = lerp(mainMenuFontHeight, height/2, 0.05);
+  mainMenuFontHeight = lerp(mainMenuFontHeight, height/2 - height/8, 0.05);
   fill(255,mainMenuFontAlpha);
   text("The Art of\nBeing Artistic",width/2,mainMenuFontHeight);
+  if (mainMenuFontAlpha >= 250 && doOnce){
+    var $button = $("<div class='button'></div>").text("START").button().click(function(){
+      state = "PLAY";
+    }).removeClass("ui-corner-all");
+    $('body').append($button);
+    doOnce = false;
+  }
   pop();
 }
