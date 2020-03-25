56bbaa4f1dd7962339798ba104ad51458e74ddc6
Tony LindoRock <yichen@Yichens-MacBook-Pro.local>
2020-03-25 17:18:10 -0400

    P3: Finally fixed the font issue and added more animation

--- Projects/P3/js/script.js
+++ Projects/P3/js/script.js
@@ -13,6 +13,7 @@ FONT FROM: https://www.urbanfonts.com/fonts/superscript.htm
 let mainMenuFontHeight;
 let mainMenuFontAlpha = 0;
 let doOnce = true;
+let titleFadeAway = false;
 
 // current state of the program
 let state = "START";
@@ -22,6 +23,9 @@ let themeFont;
 
 // the color of background
 const BG_COLOR = "#111";
+// color of highlight
+const HIGHLIGHT = "#FF7F50";
+const GREEN_BLUE = "#7FFFD4";
 
 function preload() {
   themeFont = loadFont("assets/font/SUPERSCR.TTF");
@@ -55,16 +59,25 @@ function setupMainMenu(){
 function displayMainMenu() {
   push();
   textSize(64);
-  mainMenuFontAlpha = lerp(mainMenuFontAlpha, 255, 0.05);
-  mainMenuFontHeight = lerp(mainMenuFontHeight, height/2 - height/8, 0.05);
-  fill(255,mainMenuFontAlpha);
+  if (!titleFadeAway){
+    mainMenuFontAlpha = lerp(mainMenuFontAlpha, 255, 0.05);
+    mainMenuFontHeight = lerp(mainMenuFontHeight, height/2 - height/8, 0.05);
+  }else{
+    mainMenuFontAlpha = lerp(mainMenuFontAlpha, 0, 0.05);
+    mainMenuFontHeight = lerp(mainMenuFontHeight, height + height/8, 0.03);
+  }
+  fill(127, 255, 212,mainMenuFontAlpha);
   text("The Art of\nBeing Artistic",width/2,mainMenuFontHeight);
-  if (mainMenuFontAlpha >= 250 && doOnce){
-    var $button = $("<div class='button'></div>").text("START").button().click(function(){
-      state = "PLAY";
-    }).removeClass("ui-corner-all");
+  if (mainMenuFontAlpha >= 210 && doOnce){
+    var $button = $("<div class='button' id = 'title-button'></div>").text("start").button().click(function(){
+      titleFadeAway = true;
+      $('#title-button').hide();
+    }).hide().fadeIn(500);
     $('body').append($button);
     doOnce = false;
+  }
+  if (titleFadeAway && mainMenuFontAlpha <= 0){
+    state = "TUTORIAL";
   }
   pop();
 }
